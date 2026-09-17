da4d8d12e9804273d18970e19fdf08957af3c41b
Sergey <118613573+Rysal228@users.noreply.github.com>
2026-09-18 00:27:23 +0700

    feat(orders): expose status configuration in admin

--- autoservice/orders/admin.py
+++ autoservice/orders/admin.py
@@ -1,9 +1,30 @@
 from django.contrib import admin
 
-from orders.models import Order, WorkType, OrderStatus
+from orders.models import Order, OrderStatus, WorkStatus, WorkType
 
 
+@admin.register(WorkType)
+class WorkTypeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
 
-admin.site.register(WorkType)
-admin.site.register(OrderStatus)
-admin.site.register(Order)
+
+@admin.register(OrderStatus)
+class OrderStatusAdmin(admin.ModelAdmin):
+    list_display = ('name', 'appearance', 'is_initial')
+    list_filter = ('appearance', 'is_initial')
+    search_fields = ('name',)
+
+
+@admin.register(WorkStatus)
+class WorkStatusAdmin(admin.ModelAdmin):
+    list_display = ('name', 'appearance')
+    list_filter = ('appearance',)
+    search_fields = ('name',)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('order_number', 'customer', 'car', 'work_type', 'status', 'work_status', 'appointment_at', 'price')
+    list_filter = ('status', 'work_status', 'work_type')
+    search_fields = ('order_number', 'customer__phone', 'car__plate_number')
