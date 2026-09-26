547513ec3b968f7b814f1554697153eb0c9a9daf
Черадионов Сергей Романович <cheradionovsr@cifka-k.ru>
2026-09-26 21:17:52 +0700

    build: добавление отдельного entrypoint

---
+++ autoservice/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+
+echo "Applying database migrations..."
+python manage.py migrate --noinput
+
+echo "Starting Gunicorn..."
+exec gunicorn autoservice.wsgi:application \
+    --bind 0.0.0.0:8000
